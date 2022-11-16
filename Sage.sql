581fe96fff9e6fc85f9d64cc1f0ef579709ad003
TGrl13 <113434590+TGrl13@users.noreply.github.com>
2022-11-16 18:23:28 +0100

    Add files via upload

--- Sage.sql
+++ Sage.sql
@@ -1,25 +1,25 @@
 CREATE TABLE Personne(
-   id_personne INT,
+   id_personne INT AUTO_INCREMENT,
    nom_personne VARCHAR(50) ,
    prenom_personne VARCHAR(50) ,
-   telephone TINYINT,
-   mail VARCHAR(50) ,
-   rue VARCHAR(50) ,
-   ville VARCHAR(50) ,
-   code_postal VARCHAR(50) ,
-   situation BOOLEAN,
-   nb_enfants_ INT,
+   telephone VARCHAR(50) NOT NULL,
+   mail VARCHAR(50) NOT NULL,
+   rue VARCHAR(50) NOT NULL,
+   ville VARCHAR(50) NOT NULL,
+   code_postal INT NOT NULL,
+   situation BOOLEAN NOT NULL,
+   nb_enfants_ INT NOT NULL,
    PRIMARY KEY(id_personne)
 );
 
 CREATE TABLE Catégorie_Administrative(
-   id_categorie_administrative INT,
-   nom_categorie_administrative VARCHAR(50) ,
+   id_categorie_administrative INT AUTO_INCREMENT,
+   nom_categorie_administrative VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_categorie_administrative)
 );
 
 CREATE TABLE Dossier_social(
-   id_dossier_social VARCHAR(50) ,
+   id_dossier_social INT AUTO_INCREMENT,
    id_personne INT NOT NULL,
    PRIMARY KEY(id_dossier_social),
    UNIQUE(id_personne),
@@ -27,67 +27,67 @@ CREATE TABLE Dossier_social(
 );
 
 CREATE TABLE Emplacement(
-   id_emplacement INT,
-   batiment VARCHAR(50) ,
-   piece VARCHAR(50) ,
-   numero_meuble VARCHAR(50) ,
-   etage VARCHAR(50) ,
+   id_emplacement INT AUTO_INCREMENT,
+   batiment VARCHAR(50) NOT NULL,
+   piece VARCHAR(50) NOT NULL,
+   numero_meuble INT NOT NULL,
+   etage INT NOT NULL,
    PRIMARY KEY(id_emplacement)
 );
 
 CREATE TABLE Partenaire(
-   id_partenaire VARCHAR(50) ,
-   nom_partenaire VARCHAR(50) ,
-   type_partenaire VARCHAR(50) ,
+   id_partenaire INT AUTO_INCREMENT,
+   nom_partenaire VARCHAR(50) NOT NULL,
+   type_partenaire VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_partenaire)
 );
 
 CREATE TABLE Formation(
-   id_formation INT,
-   nom_formation VARCHAR(50) ,
-   thematique VARCHAR(50) ,
-   date_formation DATE,
-   heure_debut_formation TIME,
-   heure_fin_formation TIME,
+   id_formation INT AUTO_INCREMENT,
+   nom_formation VARCHAR(50) NOT NULL,
+   thematique VARCHAR(50) NOT NULL,
+   date_formation DATE NOT NULL,
+   heure_debut_formation TIME NOT NULL,
+   heure_fin_formation TIME NOT NULL,
    PRIMARY KEY(id_formation)
 );
 
 CREATE TABLE Réunion(
-   id_reunion VARCHAR(50) ,
-   type_reunion VARCHAR(50) ,
-   date_reunion DATE,
-   heure_debut_reunion VARCHAR(50) ,
-   heure_fin_reunion VARCHAR(50) ,
+   id_reunion INT AUTO_INCREMENT,
+   type_reunion VARCHAR(50) NOT NULL,
+   date_reunion DATE NOT NULL,
+   heure_debut_reunion TIME NOT NULL,
+   heure_fin_reunion TIME NOT NULL,
    PRIMARY KEY(id_reunion)
 );
 
 CREATE TABLE Problématique(
-   id_problematique INT,
-   nom_domaine_problematique VARCHAR(50) ,
+   id_problematique INT AUTO_INCREMENT,
+   nom_domaine_problematique VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_problematique)
 );
 
 CREATE TABLE Agent_administratif(
-   id_agent INT,
-   nom_agent VARCHAR(50) ,
-   prenom_agent VARCHAR(50) ,
+   id_agent INT AUTO_INCREMENT,
+   nom_agent VARCHAR(50) NOT NULL,
+   prenom_agent VARCHAR(50) NOT NULL,
    id_categorie_administrative INT NOT NULL,
    PRIMARY KEY(id_agent),
    FOREIGN KEY(id_categorie_administrative) REFERENCES Catégorie_Administrative(id_categorie_administrative)
 );
 
 CREATE TABLE Rendez_vous(
-   id_rdv INT,
+   id_rdv INT AUTO_INCREMENT,
    date_rdv DATE NOT NULL,
-   heure_debut_rdv TIME,
-   heure_fin_rdv TIME,
-   type_prise_rdv VARCHAR(50) ,
-   motif_absence_personne VARCHAR(50) ,
-   motif_absence_agent VARCHAR(50) ,
-   nature_intervention VARCHAR(50) ,
-   id_personne INT,
-   id_agent INT,
-   id_dossier_social VARCHAR(50)  NOT NULL,
+   heure_debut_rdv TIME NOT NULL,
+   heure_fin_rdv TIME NOT NULL,
+   type_prise_rdv VARCHAR(50) NOT NULL,
+   motif_absence_personne VARCHAR(50) NOT NULL,
+   motif_absence_agent VARCHAR(50) NOT NULL,
+   nature_intervention VARCHAR(50) NOT NULL,
+   id_personne INT NOT NULL,
+   id_agent INT NOT NULL,
+   id_dossier_social INT NOT NULL,
    PRIMARY KEY(id_rdv),
    FOREIGN KEY(id_personne) REFERENCES Personne(id_personne),
    FOREIGN KEY(id_agent) REFERENCES Agent_administratif(id_agent),
@@ -95,29 +95,29 @@ CREATE TABLE Rendez_vous(
 );
 
 CREATE TABLE Document_administratif(
-   id_document VARCHAR(50) ,
-   nom_document VARCHAR(50)  NOT NULL,
+   id_document INT AUTO_INCREMENT,
+   nom_document VARCHAR(50) NOT NULL,
    categorie_document VARCHAR(50) ,
    id_emplacement INT NOT NULL,
-   id_dossier_social VARCHAR(50)  NOT NULL,
+   id_dossier_social INT NOT NULL,
    PRIMARY KEY(id_document),
    FOREIGN KEY(id_emplacement) REFERENCES Emplacement(id_emplacement),
    FOREIGN KEY(id_dossier_social) REFERENCES Dossier_social(id_dossier_social)
 );
 
 CREATE TABLE Dispositif(
-   id_dispositif VARCHAR(50) ,
-   nom_dispositif VARCHAR(50) ,
-   id_partenaire VARCHAR(50)  NOT NULL,
+   id_dispositif INT AUTO_INCREMENT,
+   nom_dispositif VARCHAR(50) NOT NULL,
+   id_partenaire INT NOT NULL,
    PRIMARY KEY(id_dispositif),
    FOREIGN KEY(id_partenaire) REFERENCES Partenaire(id_partenaire)
 );
 
 CREATE TABLE Aide(
-   id_aide VARCHAR(50) ,
-   montant INT,
-   type_aide VARCHAR(50) ,
-   id_dispositif VARCHAR(50) ,
+   id_aide INT AUTO_INCREMENT,
+   montant INT NOT NULL,
+   type_aide VARCHAR(100) NOT NULL,
+   id_dispositif INT NOT NULL,
    id_rdv INT NOT NULL,
    PRIMARY KEY(id_aide),
    FOREIGN KEY(id_dispositif) REFERENCES Dispositif(id_dispositif),
@@ -125,33 +125,37 @@ CREATE TABLE Aide(
 );
 
 CREATE TABLE Participer_formation(
-   id_agent INT,
-   id_formation INT,
-   PRIMARY KEY(id_agent, id_formation),
+   id_Participer_formation INT AUTO_INCREMENT,
+   id_agent INT NOT NULL,
+   id_formation INT NOT NULL,
+   PRIMARY KEY(id_Participer_formation),
    FOREIGN KEY(id_agent) REFERENCES Agent_administratif(id_agent),
    FOREIGN KEY(id_formation) REFERENCES Formation(id_formation)
 );
 
 CREATE TABLE Participer_réunion_2(
-   id_partenaire VARCHAR(50) ,
-   id_reunion VARCHAR(50) ,
-   PRIMARY KEY(id_partenaire, id_reunion),
+   id_Participer_réunion_2 INT AUTO_INCREMENT,
+   id_partenaire INT NOT NULL,
+   id_reunion INT NOT NULL,
+   PRIMARY KEY(id_Participer_réunion_2),
    FOREIGN KEY(id_partenaire) REFERENCES Partenaire(id_partenaire),
    FOREIGN KEY(id_reunion) REFERENCES Réunion(id_reunion)
 );
 
 CREATE TABLE Participer_réunion_1(
-   id_agent INT,
-   id_reunion VARCHAR(50) ,
-   PRIMARY KEY(id_agent, id_reunion),
+   id_Participer_réunion_1 INT AUTO_INCREMENT,
+   id_agent INT NOT NULL,
+   id_reunion INT NOT NULL,
+   PRIMARY KEY(id_Participer_réunion_1),
    FOREIGN KEY(id_agent) REFERENCES Agent_administratif(id_agent),
    FOREIGN KEY(id_reunion) REFERENCES Réunion(id_reunion)
 );
 
 CREATE TABLE Associer_problématique(
-   id_rdv INT,
-   id_problematique INT,
-   PRIMARY KEY(id_rdv, id_problematique),
+   id_Associer_problématique INT AUTO_INCREMENT,
+   id_rdv INT NOT NULL,
+   id_problematique INT NOT NULL,
+   PRIMARY KEY(id__Associer_problématique),
    FOREIGN KEY(id_rdv) REFERENCES Rendez_vous(id_rdv),
    FOREIGN KEY(id_problematique) REFERENCES Problématique(id_problematique)
 );
---
+++ populate_tables_sage_bd5.py
@@ -0,0 +1,291 @@
+from sqlalchemy import create_engine, MetaData, select
+import sqlalchemy
+from faker import Faker
+import sys
+import random
+import datetime
+import configparser
+from connect import engine
+
+
+engine = engine
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+# Instantiate faker object
+faker = Faker(['fr-FR'])
+
+Personne = metadata.tables["Personne"]
+Catégorie_Administrative = metadata.tables["Catégorie_Administrative"]
+Dossier_social = metadata.tables["Dossier_social"]
+Emplacement = metadata.tables["Emplacement"]
+Partenaire = metadata.tables["Partenaire"]
+Formation = metadata.tables["Formation"]
+Réunion = metadata.tables["Réunion"]
+Problématique = metadata.tables["Problématique"]
+Agent_administratif = metadata.tables["Agent_administratif"]
+Rendez_vous = metadata.tables["Rendez_vous"]
+Document_administratif = metadata.tables["Document_administratif"]
+Dispositif = metadata.tables["Dispositif"]
+Aide = metadata.tables["Aide"]
+Participer_formation = metadata.tables["Participer_formation"]
+Participer_réunion_2 = metadata.tables["Participer_réunion_2"]
+Participer_réunion_1 = metadata.tables["Participer_réunion_1"]
+Associer_problématique = metadata.tables["Associer_problématique"]
+
+database=[]
+
+try :
+    database.append((Personne,1000))
+    database.append((Catégorie_Administrative,15))
+    database.append((Dossier_social,1000))
+    database.append((Emplacement,500))
+    database.append((Partenaire,500))
+    database.append((Formation,1000))
+    database.append((Réunion,50))
+    database.append((Problématique,500))
+    database.append((Agent_administratif,500))
+    database.append((Rendez_vous,100))
+    database.append((Document_administratif,100))
+    database.append((Dispositif,100))
+    database.append((Aide,50))
+    database.append((Participer_formation,50))
+    database.append((Participer_réunion_2,50))
+    database.append((Participer_réunion_1,50))
+    database.append((Associer_problématique,50))
+
+except KeyError as err:
+    print("error : Metadata.tables "+str(err)+" not found")
+
+# Liste catégorie administrative 
+cat_admin = ["A", "B", "C"]
+
+thematique_formation = ["New Comer", "Social", "Manager", "Juridique", "Sécurité", "Finance"]
+
+nom_formation = ["Formation1", "Formation2", "Formation3", "Formation4", "Formation5", "Formation6"]
+
+nom_piece = ["Piece", "Piece2", "Piece3", "Piece4", "Piece5", "Piece6"]
+
+nom_batiment = ["Batiment1", "Batiment2", "Batiment3", "Batiment4", "Batiment5", "Batiment6"]
+
+type_partenaire = ["Commune", "Etat", "Association", "Departement", "Entreprise", "Région"]
+
+type_reunion = ["Par Téléphone", "En visio", "En presentiel"]
+
+type_absence = ["Maladie", "Erreur de planning", "Bug informatique", "Retard", "Raison personnel", "Grève"]
+
+nature_rendez_vous = ["1er RDV", "Suivie dossier"]
+
+type_aide = ["Logement", "Financière", "Sociale", "Transport"]
+
+cat_doc = ["Identité", "Domicile", "Compte bancaire", "Revenus", "Famille", "Santé"]
+
+nom_doc = ["CNI", "Justificatif de domicile", "RIB", "Déclaration d'impot", "Passeport", "Livret de famille", "NSS"]
+
+nom_dispositif_list = ["Allocations familiales", "Prestation d’accueil du jeune enfant", "Allocation de rentrée scolaire",
+    "L’ Allocation Personnalisée d’ Autonomie APA", "Aide au permis de conduire", "APL"]
+
+class GenerateData:
+    """
+    generate a specific number of records to a target table
+    """
+
+    def __init__(self,table):
+        """
+        initialize command line arguments
+        """
+        self.table = table[0]
+        self.num_records = table[1]
+
+
+    def create_data(self):
+        """
+        using faker library, generate data and execute DML
+        """
+
+        if self.table.name == "Personne":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_personne = faker.last_name(),
+                        prenom_personne = faker.first_name(),
+                        telephone = faker.phone_number(),
+                        mail = faker.email(),
+                        rue = faker.building_number(),
+                        ville = faker.city(),
+                        code_postal = faker.postcode(),
+                        situation = faker.boolean(chance_of_getting_true=50),
+                        nb_enfants_ = faker.random_int(0, 10)
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "Catégorie_Administrative":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_categorie_administrative = random.choice(cat_admin)
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "Dossier_social":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        personnes = conn.execute(select([Personne.c.id_personne])).fetchall()
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Emplacement":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        batiment = random.choice(nom_batiment),
+                        piece = random.choice(nom_piece),
+                        numero_meuble = faker.random_int(1,200),
+                        etage = faker.random_int(0, 10)
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Partenaire":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_partenaire = faker.company(),
+                        type_partenaire = random.choice(type_partenaire)
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Formation":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    date_obj = datetime.datetime.now() - datetime.timedelta(days=random.randint(0,30))
+                    insert_stmt = self.table.insert().values(
+                        nom_formation = random.choice(nom_formation),
+                        thematique = random.choice(thematique_formation),
+                        date_formation = date_obj.strftime("%Y/%m/%d"),
+                        heure_debut_formation = faker.time("%H:%M%S"),
+                        heure_fin_formation = faker.time("%H:%M%S")
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Réunion":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    date_obj = datetime.datetime.now() - datetime.timedelta(days=random.randint(0,30))
+                    insert_stmt = self.table.insert().values(
+                        type_reunion = random.choice(type_reunion),
+                        date_reunion = date_obj.strftime("%Y/%m/%d"),
+                        heure_debut_reunion = faker.time("%H:%M%S"),
+                        heure_fin_reunion = faker.time("%H:%M%S")
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Problématique":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_domaine_problematique = random.choice(type_aide)
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Agent_administratif":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_agent = faker.first_name(),
+                        prenom_agent = faker.last_name(),
+                        id_categorie_administrative = random.choice(conn.execute(select([Catégorie_Administrative.c.id_categorie_administrative])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Rendez_vous":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    date_obj = datetime.datetime.now() - datetime.timedelta(days=random.randint(0,30))
+                    insert_stmt = self.table.insert().values(
+                        date_rdv =  date_obj.strftime("%Y/%m/%d"),
+                        heure_debut_rdv = faker.time("%H:%M%S"),
+                        heure_fin_rdv = faker.time("%H:%M%S"),
+                        type_prise_rdv = random.choice(type_reunion),
+                        motif_absence_personne =  random.choice(type_absence),
+                        motif_absence_agent = random.choice(type_absence),
+                        nature_intervention = random.choice(nature_rendez_vous),
+                        id_personne = random.choice(conn.execute(select([Personne.c.id_personne])).fetchall())[0],
+                        id_agent = random.choice(conn.execute(select([Agent_administratif.c.id_agent])).fetchall())[0],
+                        id_dossier_social = random.choice(conn.execute(select([Dossier_social.c.id_dossier_social])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Document_administratif":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_document = random.choice(nom_doc),
+                        categorie_document =  random.choice(cat_doc),
+                        id_emplacement = random.choice(conn.execute(select([Emplacement.c.id_emplacement])).fetchall())[0],
+                        id_dossier_social = random.choice(conn.execute(select([Dossier_social.c.id_dossier_social])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Dispositif":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        nom_dispositif = random.choice(nom_dispositif_list),
+                        id_partenaire = random.choice(conn.execute(select([Partenaire.c.id_partenaire])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+        
+        if self.table.name == "Aide":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        montant = random.randint(100,2000),
+                        type_aide = random.choice(type_aide),
+                        id_dispositif = random.choice(conn.execute(select([Dispositif.c.id_dispositif])).fetchall())[0],
+                        id_rdv = random.choice(conn.execute(select([Rendez_vous.c.id_rdv])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Participer_formation":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        id_agent = random.choice(conn.execute(select([Agent_administratif.c.id_agent])).fetchall())[0],
+                        id_formation = random.choice(conn.execute(select([Formation.c.id_formation])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Participer_réunion_2":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        id_partenaire = random.choice(conn.execute(select([Partenaire.c.id_partenaire])).fetchall())[0],
+                        id_reunion = random.choice(conn.execute(select([Réunion.c.id_reunion])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Participer_réunion_1":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        id_agent = random.choice(conn.execute(select([Agent_administratif.c.id_agent])).fetchall())[0],
+                        id_reunion = random.choice(conn.execute(select([Réunion.c.id_reunion])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+                    
+        if self.table.name == "Associer_problématique":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        id_rdv = random.choice(conn.execute(select([Rendez_vous.c.id_rdv])).fetchall())[0],
+                        id_problematique = random.choice(conn.execute(select([Problématique.c.id_problematique])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+        
+
+
+if __name__ == "__main__":
+    for i in database:
+        generate_data = GenerateData(i)
+        generate_data.create_data()
