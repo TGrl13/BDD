0ea5e6eaeefad5fef74a8c29181f3a022d6d7f77
TGrl13 <113434590+TGrl13@users.noreply.github.com>
2022-11-16 09:43:12 +0100

    Add files via upload

--- Collabo.sql
+++ Collabo.sql
@@ -1,27 +1,28 @@
 CREATE TABLE Enregistrement(
-   Numero_de_ticket VARCHAR(50) ,
+   Numero_de_ticket INT AUTO_INCREMENT,
    Date_heure DATETIME,
    PRIMARY KEY(Numero_de_ticket)
 );
 
 CREATE TABLE Medecin(
-   Numéro_RPPS VARCHAR(50) ,
-   Nom_medecin VARCHAR(50) ,
-   Adresse_postal VARCHAR(50) ,
-   Mail_medecin VARCHAR(50) ,
+   Numéro_RPPS INT AUTO_INCREMENT,
+   Nom_medecin VARCHAR(50),
+   Prenom_medecin VARCHAR(50),
+   Adresse_postal VARCHAR(100),
+   Mail_medecin VARCHAR(50),
    PRIMARY KEY(Numéro_RPPS)
 );
 
 CREATE TABLE Patient(
-   Numero_patient INT,
-   Nom_patient VARCHAR(50) ,
-   Prenom_patient VARCHAR(50) ,
+   Numero_patient INT AUTO_INCREMENT,
+   Nom_patient VARCHAR(50),
+   Prenom_patient VARCHAR(50),
    Date_de_naissance DATE,
-   Adresse_patient VARCHAR(50) ,
-   Poids_patient DECIMAL(15,2)  ,
-   Taille_patient DECIMAL(15,2)  ,
-   NSS_patient VARCHAR(50) ,
-   Medecin_traitant VARCHAR(50) ,
+   Adresse_patient VARCHAR(100),
+   Poids_patient DECIMAL(3,1),
+   Taille_patient INT NOT NULL,
+   NSS_patient VARCHAR(50),
+   Medecin_traitant VARCHAR(50),
    Autorisation_partage BOOLEAN,
    Numéro_RPPS VARCHAR(50)  NOT NULL,
    PRIMARY KEY(Numero_patient),
@@ -29,21 +30,22 @@ CREATE TABLE Patient(
 );
 
 CREATE TABLE Analyse(
-   Numéro_analyse INT,
+   Numéro_analyse INT AUTO_INCREMENT,
    Date_analyse DATE,
    Heure_analyse TIME,
-   Type_analyse VARCHAR(50) ,
-   Forfait_analyse VARCHAR(50) ,
-   Vecteur_analyse VARCHAR(50) ,
+   Type_analyse VARCHAR(50),
+   Forfait_analyse VARCHAR(50),
+   Vecteur_analyse VARCHAR(50),
    Numero_patient INT NOT NULL,
    PRIMARY KEY(Numéro_analyse),
    FOREIGN KEY(Numero_patient) REFERENCES Patient(Numero_patient)
 );
 
 CREATE TABLE S_enregistre(
-   Numero_patient INT,
-   Numero_de_ticket VARCHAR(50) ,
-   PRIMARY KEY(Numero_patient, Numero_de_ticket),
+   Id_S_enregistre INT AUTO_INCREMENT,
+   Numero_patient INT NOT NULL,
+   Numero_de_ticket VARCHAR(50),
+   PRIMARY KEY(Id_S_enregistre),
    FOREIGN KEY(Numero_patient) REFERENCES Patient(Numero_patient),
    FOREIGN KEY(Numero_de_ticket) REFERENCES Enregistrement(Numero_de_ticket)
 );
---
+++ populate_tables_2.py
@@ -0,0 +1,121 @@
+from sqlalchemy import create_engine, MetaData, select
+from faker import Faker
+import sys
+import random
+import datetime
+import configparser
+from connect import engine
+
+engine = engine
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+# Instantiate faker object
+faker = Faker(['fr-FR'])
+
+Enregistrement = metadata.tables["Enregistrement"]
+Medecin = metadata.tables["Medecin"]
+Patient = metadata.tables["Patient"]
+Analyse = metadata.tables["Analyse"]
+S_enregistre = metadata.tables["S_enregistre"]
+
+database=[]
+
+try :
+    database.append((Enregistrement,2000))
+    database.append((Medecin,2000))
+    database.append((Patient,2000))
+    database.append((Analyse,2000))
+    database.append((S_enregistre,2000))
+except KeyError as err:
+    print("error : Metadata.tables "+str(err)+" not found")
+
+# rvecteur list
+vecteur_list = ["Prélèvement sanguin", "Prélèvement salivaire", "Prélèvement urinaire", "Prélèvement broncho-pulmonaires",
+    "prélèvement de liquide gastrique", "prélèvement cutané", "Prélèvement de pus", "prélèvement uro-génital"]
+
+# type list
+type_list = ["VIH", "Thyroïde", "Shyphilis", "Mucoviscidose", "allergies alimentaires", "cholestérole", "globules rouges", "glycémie"
+    "magnésium", "fer", "MST", "test de paternité"]
+
+class GenerateData:
+    """
+    generate a specific number of records to a target table
+    """
+
+    def __init__(self,table):
+        """
+        initialize command line arguments
+        """
+        self.table = table[0]
+        self.num_records = table[1]
+
+
+    def create_data(self):
+        """
+        using faker library, generate data and execute DML
+        """
+
+        if self.table.name == "Enregistrement":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        Date_heure = faker.date_time().strftime("%Y/%m/%d, %H:%M:%S"),
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "Medecin":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        Nom_medecin = faker.last_name(),
+                        Prenom_medecin = faker.first_name(),
+                        Adresse_postal = faker.address(),
+                        Mail_medecin = faker.email(),
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "Patient":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    date_ins = datetime.datetime.now() - datetime.timedelta(days=random.randint(0,30))
+                    insert_stmt = self.table.insert().values(
+                        Nom_patient = faker.last_name(),
+                        Prenom_patient = faker.first_name(),
+                        Date_de_naissance = faker.date_of_birth(minimum_age=1, maximum_age=120),
+                        Adresse_patient = faker.address(),
+                        Poids_patient = faker.pydecimal(left_digits=None, right_digits=1, positive=True, min_value=2.5, max_value=200),
+                        Taille_patient = faker.random_int(45, 250),
+                        NSS_patient = faker.ssn(),
+                        Medecin_traitant = faker.name(),
+                        Autorisation_partage = faker.boolean(chance_of_getting_true=50),
+                        Numéro_RPPS = random.choice(conn.execute(select([ Medecin.c.Numéro_RPPS])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "Analyse":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        Date_analyse = faker.date().strftime("%Y/%m/%d"),
+                        Heure_analyse = faker.time().strftime("%H/%M/%S"),
+                        Type_analyse = random.choice(type_list),
+                        Forfait_analyse = random.choice(type_list) + ", " + random.choice(type_list) + ", " + random.choice(type_list),
+                        Vecteur_analyse = random.choice(vecteur_list),
+                        Numero_patient=random.choice(conn.execute(select([Patient.c.Numero_patient])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+
+        if self.table.name == "S_enregistre":
+            with engine.begin() as conn:
+                for _ in range(self.num_records):
+                    insert_stmt = self.table.insert().values(
+                        Numero_patient = random.choice(conn.execute(select([Patient.c.Numero_patient])).fetchall())[0],
+                        Numero_de_ticket = random.choice(conn.execute(select([Enregistrement.c.Numero_de_ticket])).fetchall())[0]
+                    )
+                    conn.execute(insert_stmt)
+
+if __name__ == "__main__":
+    for i in database:
+        generate_data = GenerateData(i)
+        generate_data.create_data()
